61fe75f0a63a0f299977ea1e3d63722587308afd
admsk2 <kozlowski001@yahoo.com>
2016-07-04 13:07:39 +0200

    Prototype vol.1

--- every.js
+++ every.js
@@ -1,1 +1,48 @@
-module.exports = function() {}
+module.exports = {
+    five: function (value, scope) {},
+
+    six: function (value, scope) {
+        var t = ['*','*','*','*','*','*'],
+            v, s, r;
+
+        let _v = function (value) {
+            if (typeof scope === 'string') {
+                v = parseInt(value);
+            } else if (typeof scope === 'number') {
+                v = value;
+            } else {
+                let err = new Error('First parameter has to be a string or number.');
+                return err;  
+            }
+        }
+
+        _v(value);
+
+        let _s = function (scope) {
+            if (typeof scope !== 'string') {
+                let err = new Error('Second parameter has to be a string.');
+                return err;
+            } else {
+                s = scope;
+            }
+        }
+        
+        _s(scope);
+        
+        
+        
+        _r = function() {
+            switch (s):
+                case ('second' || 'seconds'):
+                    r = function() {
+                    let _t = t;
+                    (s == 30) ? _t[0] = '00, 30' : _t[0] = '*/'+s;
+                    }
+                break;  
+        }
+
+        //
+        return r;
+
+    }
+}
