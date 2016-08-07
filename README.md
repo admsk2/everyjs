a5daa80eeefb9c3e8881b7d9c1d315afe2217f1a
admsk2 <kozlowski001@yahoo.com>
2016-08-07 17:31:26 +0200

    Version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # everyjs
-Basically, use it instead of ' * * * * * * ' when setting up CRON in nodeJS.
+> v.0.1.0
+
+The idea is to use it instead of '* * * * * *' when setting up CRON in nodeJS.
+
+node-cron info:
+[https://github.com/ncb000gt/node-cron](https://github.com/ncb000gt/node-cron)
+
+### Important
+This version does not cover patterns which are higher than half of maximum value. For example, you can not set **CRON** every 45 minutes, 55 seconds, 15 hours, 7 months etc. This functionality is still in progress.
+
+### About
+
+Usually, node-cron is called following way:
+
+```
+var job = new CronJob('00 30 11 * * 1-5');
+
+or
+
+var job = new CronJob({
+  cronTime: '00 30 11 * * 1-5'
+});
+```
+
+You may find this pattern hard to understand, same as i had.
+
+So with everyjs you can set up the CronJob like:
+
+```
+var job = new CronJob(every(5, 'minutes'));
+
+or
+
+var job = new CronJob({
+  cronTime: every(5, 'minutes')
+});
+```
+
+Way more readable.
+
+### Rules
+
+You have following options to choose:
+
+1. Seconds - _every(1-30, 'seconds')_
+2. Minutes - _every(1-30, 'minutes')_
+3. Hours - _every(1-12, 'hours')_
+4. Days - _every(1-15, 'days')_
+5. Months - _every(1-6, 'months')_
+*6. Days Of Week - _every('monday')_
+
+*look usage below, this one requires different method
+
+The return of this method is a CRON pattern with proper rule. e.g. _'*/30 * * * * *'_ for _every(30, 'seconds)_
+
+### Usage
+
+Just remember to require the module at the beginning of your .js file.
+
+```
+every = require('everyjs').general // for options 1-5
+
+or
+
+every = require('everyjs').daysOfWeek // for option 6
+```
+
+**That is it. Hope it helps!**
--- every.js
+++ every.js
@@ -1,18 +1,47 @@
+"use strict";
 module.exports = {
-    five: function (value, scope) {},
+    daysOfWeek: function (value, scope) {
+        var t = ['*', '*', '*', '*', '*', '*'],
+            v, r;
 
-    six: function (value, scope) {
-        var t = ['*','*','*','*','*','*'],
+        let _v = function (value) {
+            if (typeof value !== 'string' && (value > 7 || value < 1)) {
+                let err = new Error('Please choose days from 1-7. First day is monday.');
+                return err;
+            } else {
+                v = value;
+            }
+        }
+
+        _v(value);
+
+        let _r = function () {
+            if (v.indexOf('monday') > -1 || v === 1) {t[5] = '1';}
+            else if (v.indexOf('tuesday') > -1 || v === 2) {t[5] = '2';}
+            else if (v.indexOf('wednesday') > -1 || v === 3) {t[5] = '3';}
+            else if (v.indexOf('thursday') > -1 || v === 3) {t[5] = '4';}
+            else if (v.indexOf('friday') > -1 || v === 3) {t[5] = '5';}
+            else if (v.indexOf('saturday') > -1 || v === 3) {t[5] = '6';}
+            else if (v.indexOf('sunday') > -1 || v === 3) {t[5] = '7';}
+        }
+
+        _r();
+        r = '' + t.join(" ");
+        return r;
+    },
+
+    general: function (value, scope) {
+        var t = ['*', '*', '*', '*', '*', '*'],
             v, s, r;
 
         let _v = function (value) {
-            if (typeof scope === 'string') {
+            if (typeof value === 'string') {
                 v = parseInt(value);
-            } else if (typeof scope === 'number') {
+            } else if (typeof value === 'number') {
                 v = value;
             } else {
                 let err = new Error('First parameter has to be a string or number.');
-                return err;  
+                return err;
             }
         }
 
@@ -26,23 +55,52 @@ module.exports = {
                 s = scope;
             }
         }
-        
+
         _s(scope);
-        
-        
-        
-        _r = function() {
-            switch (s):
-                case ('second' || 'seconds'):
-                    r = function() {
-                    let _t = t;
-                    (s == 30) ? _t[0] = '00, 30' : _t[0] = '*/'+s;
-                    }
-                break;  
+
+        let _r = function () {
+            if (s.indexOf('second') > -1) {
+                if (v > 30 || v === 0) {
+                    let err = new Error('Invalid value');
+                    return err;
+                } else {
+                    (v === 30) ? t[0] = '00,30': t[0] = '*/' + v;
+                }
+
+            } else if (s.indexOf('minute') > -1) {
+                if (v > 30 || v === 0) {
+                    let err = new Error('Invalid value');
+                    return err;
+                } else {
+                    (v === 30) ? t[1] = '00,30': t[1] = '*/' + v;
+                }
+            } else if (s.indexOf('hour') > -1) {
+                if (v > 12 || v === 0) {
+                    let err = new Error('Invalid value');
+                    return err;
+                } else {
+                    (v === 12) ? t[2] = '00,12': t[2] = '*/' + v;
+                }
+            } else if (s.indexOf('day') > -1) {
+                if (v > 15 || v === 0) {
+                    let err = new Error('Invalid value');
+                    return err;
+                } else {
+                    (v === 15) ? t[3] = '00,15': t[3] = '*/' + v;
+                }
+            } else if (s.indexOf('month') > -1) {
+                if (v > 6 || v === 0) {
+                    let err = new Error('Invalid value');
+                    return err;
+                } else {
+                    (v === 6) ? t[4] = '00,6': t[4] = '*/' + v;
+                }
+            }
         }
 
-        //
+        _r();
+        r = '' + t.join(" ");
         return r;
-
     }
+
 }
